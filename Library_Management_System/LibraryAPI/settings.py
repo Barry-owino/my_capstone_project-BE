bca5d915caad42a991d1a7e6e2d6715b7f89a913
Barry-owino <barryowino5@gmail.com>
2025-03-21 22:24:16 +0300

    created the required apps added config to the setting.py

--- Library_Management_System/LibraryAPI/settings.py
+++ Library_Management_System/LibraryAPI/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books_app',
+    'users_app',
+    'transactions_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
