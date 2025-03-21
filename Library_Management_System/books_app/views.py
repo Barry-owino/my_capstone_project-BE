ba4967191c4fd8c802d2e1f9f00e4e9b159f4faf
Barry-owino <barryowino5@gmail.com>
2025-03-21 23:17:03 +0300

    created views for CRUD functionality for the book_app

--- Library_Management_System/books_app/views.py
+++ Library_Management_System/books_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# this view class list all books or create a new book.
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+#this view retrieve, update, or delete a single book
+class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
