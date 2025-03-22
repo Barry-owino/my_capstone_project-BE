a34949190ddd65ca9f02fb5d5bc498621e611434
Barry-owino <barryowino5@gmail.com>
2025-03-22 21:39:56 +0300

    define the user model and the serializers inside the user_app

--- Library_Management_System/books_app/models.py
+++ Library_Management_System/books_app/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
     published_date = models.DateField()
-    copies_available = models.IntegerField()
+    copies_available = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
