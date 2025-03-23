56d0c390693a77afbfd599084b8c0937c424ef58
Barry-owino <barryowino5@gmail.com>
2025-03-23 12:35:36 +0300

    defined a BookCheckout model inside the transaction_app

--- Library_Management_System/transactions_app/models.py
+++ Library_Management_System/transactions_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from books_app.models import Book
 
 # Create your models here.
+
+class BookCheckout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField(default=now)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
