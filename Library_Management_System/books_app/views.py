f50718d9bc1cf761b62aa08a27b5b1b971d689db
Barry-owino <barryowino5@gmail.com>
2025-04-01 20:35:53 +0300

    implemented permissions to the BookRetrieveUpdateDestroy view

--- Library_Management_System/books_app/views.py
+++ Library_Management_System/books_app/views.py
@@ -1,5 +1,5 @@
 #from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
@@ -24,3 +24,24 @@ class BookListCreate(generics.ListCreateAPIView):
 class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #allows only authenticated users to modify
+
+    def perform_update(self, serializer):
+        """prevent updating ISBN and ensure copies are available before updating"""
+        instance = self.get_object()
+
+        #prevent isbn updates
+        if 'isbn' in self.request.data and self.request.data['isbn'] != instance.isbn:
+            raise ValidationError({"isbn": "You cannot change the ISBN of a book."})
+
+        #ensure book is available before updating
+        if 'copies_available' in self.request.data and int(self.request.data['copies_available']) < 0:
+            raise ValidationError({"copie_available": "Copies available cannot be negative."})
+        
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        #restrict book deletion to admin users only
+        if not self.request.user.is_staff:
+            raise ValidationError({"detail": "Only admin users can delete books"})
+        instance.delete()
